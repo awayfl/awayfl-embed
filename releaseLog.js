82412afcfc578421a75a30edbe75bbeabad5f7ac
Konstantin Timoshenko <rondo.devil@gmail.com>
2021-02-10 16:04:07 +0300

    Release log history of diff update

---
+++ releaseLog.js
@@ -0,0 +1,74 @@
+import { execFile } from 'child_process';
+
+function invoke(file = '', args = [], options) {
+    return new Promise((res, rej) => {
+        execFile(file, args, options, (err, out, steerr) => {
+            if (err) {
+                rej(err);
+            }
+            
+            if (steerr) {
+                rej(steerr);
+            }
+
+            res(out);
+        });
+    });
+}
+
+function collectPreviouTagAfterHead (repoUrls = []) {
+    const req = repoUrls.map(({url}) => {
+        return invoke(
+            'git', 
+            ['tag', '--sort=-taggerdate'],
+            { cwd: url, windowsHide: true }
+        ).then((list) => {
+            const tags = list.split('\n').filter(Boolean);
+            return tags[1] || tags[0];
+        });
+    });
+
+    return Promise.all(req);
+}
+
+function collectLastTags (repoUrls = []) {
+    const req = repoUrls.map(({url}) => {
+        return invoke(
+            'git', 
+            ['describe', '--abbrev=0', '--tags'],
+            { cwd: url, windowsHide: true }
+        ).then((e) => e.trim());
+    });
+
+    return Promise.all(req);
+}
+
+function collectCommits (repoUrls = [], endTags = []) {
+    const req = repoUrls.map(({url}, i) => {
+        return invoke(
+            'git', 
+            ['log', '--pretty=format:"%cd; %s"','--date=short', `HEAD...${endTags[i].trim()}`],
+            { cwd: url, windowsHide: true }
+        ).then(h => h.split('\n').filter(Boolean).map((l) => l.substring(1, l.length - 1)));
+    });
+
+    return Promise.all(req);
+}
+
+
+export async function releaseLog (reposList = [], endTags = []) {
+    if (!endTags || endTags.length === 0) {
+        endTags = await collectPreviouTagAfterHead(reposList);
+    }
+
+    const currentTags = await collectLastTags(reposList);
+    const history = await collectCommits(reposList, endTags);
+    
+    return reposList.map((e, i) => ({
+        currentTag: currentTags[i],
+        endTag: endTags[i].trim(), 
+        commits: history[i],
+        url: e.url,
+        name: e.name
+    }));
+}
--- rollup.config.js
+++ rollup.config.js
@@ -2,133 +2,163 @@ import typescript from 'rollup-plugin-typescript2'
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
-import replace  from '@rollup/plugin-replace'
-import { string as strings } from 'rollup-plugin-string';
+import replace  from '@rollup/plugin-replace';
+import { string } from 'rollup-plugin-string';
+import { releaseLog } from './releaseLog';
 import fs from 'fs';
 
 const p = require('./package.json');
-const VERSION = p.version;
-const DEPS = p.dependencies;
-const REAL_VERS = {};
 
-const versions = Object.keys(DEPS).map( (name) => {
-	const pstring = fs.readFileSync(`./node_modules/${name}/package.json`);
-	const p = JSON.parse(pstring);
+export default async (args) => {
+	const VERSION = p.version;
+	const DEPS = p.dependencies;
+	const REAL_VERS = {};
 
-	REAL_VERS[name] = p.version;
-	return `${name}:${p.version}`;
-});
+	const depsRegister = Object.keys(DEPS)
+		.filter(name => name.indexOf('away') > -1)
+		.map( (name) => ({
+			name: name,
+			url: `./node_modules/${name}`
+		}));
 
-const emit = [
-	`Bundle version: ${VERSION}`,
-	`Build at: ${new Date()}`,
-	'='.repeat(10),
-	'Modules:',
-	...versions
-];
+	const versions = depsRegister.map((e) => {
+		const pstring = fs.readFileSync(`${e.url}/package.json`);
+		const p = JSON.parse(pstring);
 
-fs.writeFileSync('./dist/VERSIONS.txt', emit.join('\n'));
+		REAL_VERS[e.name] = p.version;
+		return `${e.name}:${p.version}`;
+	});
 
-const EMBED_CONFIG = {
-	input: './src/embed/index.ts',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'AwayEmbed',
-		file: './dist/embed.js'
-	},
-	plugins: [
-		strings({
-			include: "**/*.html",
-		}),
-		replace({
-			__VERSION__: VERSION,
-			__DATE__: new Date().toDateString(),
-		}), 
-		typescript({
-			rollupCommonJSResolveHack:true,
-			tsconfigOverride: {
-				compilerOptions: {
-					target: 'es2017'
+	const versionMapped = [
+		`Bundle version: ${VERSION}`,
+		`Build at: ${new Date()}`,
+		'='.repeat(10),
+		'Modules:',
+		...versions
+	];
+	
+	const tags = p.lastUsedTags 
+		? depsRegister.map((e) => p.lastUsedTags[e.name]) 
+		: undefined;
+
+	const releaseLogs = await releaseLog(depsRegister, tags);
+	const releaseLogsMapped = releaseLogs
+		.filter((e) => e.commits.length > 0)
+		.map((e) => {
+			// remove top 2 commits: tag and update version commit
+			let commits = e.commits.length > 2 ? e.commits.slice(2) : e.commits;
+			commits = commits.filter((e) => e.indexOf('Merge branch') === -1);
+
+			return `${e.name} (${e.currentTag}-${e.endTag}):\n\t${commits.join('\n\t')}`;
+		});
+
+	fs.writeFileSync('./dist/VERSIONS.txt', versionMapped.join('\n'));
+	fs.writeFileSync('./dist/HISTORY.txt', releaseLogsMapped.join('\n\n'));
+
+	p.lastUsedTags = releaseLogs.reduce((acc, e) => (acc[e.name] = e.currentTag, acc) , {});
+
+	fs.writeFileSync('./package.json', JSON.stringify(p, null, 2));
+	
+	//return;
+	const EMBED_CONFIG = {
+		input: './src/embed/index.ts',
+		output: {
+			sourcemap: true,
+			format: 'iife',
+			name: 'AwayEmbed',
+			file: './dist/embed.js'
+		},
+		plugins: [
+			string({
+				include: "**/*.html",
+			}),
+			replace({
+				__VERSION__: VERSION,
+				__DATE__: new Date().toDateString(),
+			}), 
+			typescript({
+				rollupCommonJSResolveHack:true,
+				tsconfigOverride: {
+					compilerOptions: {
+						target: 'es2017'
+					}
 				}
-			}
-		}),
-		nodeResolve({
-			mainFields: ['jsnext', 'module']
-		}),
-		commonjs({
-			include: /node_modules/,
-		}),
-		terser({})
-	]
-};
+			}),
+			nodeResolve({
+				mainFields: ['jsnext', 'module']
+			}),
+			commonjs({
+				include: /node_modules/,
+			}),
+			terser({})
+		]
+	};
 
-const LOADER_CONFIG = {
-	input: './src/loader/index.ts',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'AWAYFL',
-		file: './dist/loader.js'
-	},
-	plugins: [
-		replace({
-			__VERSION__: VERSION,
-			__DATE__: new Date().toDateString(),
-		}), 
-		typescript({
-			rollupCommonJSResolveHack:true,
-			tsconfigOverride: {
-				compilerOptions: {
-					target: 'es2017'
+	const LOADER_CONFIG = {
+		input: './src/loader/index.ts',
+		output: {
+			sourcemap: true,
+			format: 'iife',
+			name: 'AWAYFL',
+			file: './dist/loader.js'
+		},
+		plugins: [
+			replace({
+				__VERSION__: VERSION,
+				__DATE__: new Date().toDateString(),
+			}), 
+			typescript({
+				rollupCommonJSResolveHack:true,
+				tsconfigOverride: {
+					compilerOptions: {
+						target: 'es2017'
+					}
 				}
-			}
-		}),
-		nodeResolve({
-			mainFields: ['jsnext', 'module']
-		}),
-		commonjs({
-			include: /node_modules/,
-		}),
-		terser({})
-	]
-};
+			}),
+			nodeResolve({
+				mainFields: ['jsnext', 'module']
+			}),
+			commonjs({
+				include: /node_modules/,
+			}),
+			terser({})
+		]
+	};
 
-const RUNTIME_CONFIG = 
-{
-	input: './src/runtime.ts',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'AWAYFL',
-		file: './dist/runtime.js'
-	},
-	plugins: [
-		replace({
-			__VERSION__: VERSION,
-			__LIBS__: JSON.stringify(JSON.stringify(REAL_VERS)),
-			__DATE__: new Date().toDateString()
-		}),
-		typescript({
-			rollupCommonJSResolveHack:true,
-		}),
-		nodeResolve({
-			mainFields: ['jsnext', 'module']
-		}),
-		commonjs({
-			include: /node_modules/,
-			namedExports: {
-				'random-seed': [ 'create' ],
-				'../@awayjs/scene/node_modules/he/he.js': ['decode'],
-				'../@awayfl/avm1/node_modules/random-seed/index.js':['create']
-			},
-		}),
-		terser({})
-	]
-};
+	const RUNTIME_CONFIG = 
+	{
+		input: './src/runtime.ts',
+		output: {
+			sourcemap: true,
+			format: 'iife',
+			name: 'AWAYFL',
+			file: './dist/runtime.js'
+		},
+		plugins: [
+			replace({
+				__VERSION__: VERSION,
+				__LIBS__: JSON.stringify(JSON.stringify(REAL_VERS)),
+				__DATE__: new Date().toDateString()
+			}),
+			typescript({
+				rollupCommonJSResolveHack:true,
+			}),
+			nodeResolve({
+				mainFields: ['jsnext', 'module']
+			}),
+			commonjs({
+				include: /node_modules/,
+				namedExports: {
+					'random-seed': [ 'create' ],
+					'../@awayjs/scene/node_modules/he/he.js': ['decode'],
+					'../@awayfl/avm1/node_modules/random-seed/index.js':['create']
+				},
+			}),
+			terser({})
+		]
+	};
 
 
-export default (args) => {
 	const conf = [];
 
 	if(args.embed) {
@@ -149,5 +179,5 @@ export default (args) => {
 	if (conf.length === 0)
 		conf.push(EMBED_CONFIG, LOADER_CONFIG, RUNTIME_CONFIG);
 
-	return conf
+	return conf;
 }
