f67848a4c5f5153281faab305939461f5f0fb969
nosamu <71368227+n0samu@users.noreply.github.com>
2023-07-19 13:49:03 -0500

    Update almost complete

--- src/embed/index.ts
+++ src/embed/index.ts
@@ -3,11 +3,11 @@ import template from "./embed.html";
 import {AwayPlayerComponent} from './AwayPlayerComponent';
 
 interface IAWAY_EMBED_CFG {
-    loaderUrl: string,
+    loaderUrl?: string,
     baseUrl?: string,
     runtimeUrl?: string,
     trackEmbedObjectTags?: boolean,
-    splash?: string,
+    splash?: string | boolean,
     progress?: {
 		back: string;
 		rect: [number, number, number, number],
@@ -17,14 +17,16 @@ interface IAWAY_EMBED_CFG {
 }
 
 const NOT_IMPLEMENTED = (name) =>
-    console.warn("AwayFL loader not support " + name);
+    console.warn("AwayFL loader does not support " + name);
+
+const scriptSrc = (<HTMLScriptElement>document.currentScript).src;
 
 const global = <any>window;
 
 let AWAY_EMBED_CFG: IAWAY_EMBED_CFG;
 
 if (global.swfObject) {
-    console.warn("Replace `swfObject` to AwayFl loader!");
+    console.warn("Replace `swfObject` with AwayFl loader!");
 }
 
 function createRuntimeFrame(
@@ -33,29 +35,20 @@ function createRuntimeFrame(
     width: string, 
     height: string) {
 
+    if (!global.AWAY_EMBED_CFG) {
+        console.info('AwayFL configuration not found. To set custom options, define a `AWAY_EMBED_CFG` object as explained in the Readme');
+        global.AWAY_EMBED_CFG = {};
+    }
     AWAY_EMBED_CFG = global.AWAY_EMBED_CFG;
 
-    if (!AWAY_EMBED_CFG) {
-        throw 'AwayFL configuration not found, insert define a `AWAY_AMBED_CFG` as in wiki';
-    }
-
-    if(!AWAY_EMBED_CFG.loaderUrl) {
-        throw 'AwayFL loader not defined! Runtime will not load correctly!';
-    }
-
-    const loaderUrl = AWAY_EMBED_CFG.loaderUrl;
     let baseUrl = AWAY_EMBED_CFG.baseUrl;
+    let loaderUrl = AWAY_EMBED_CFG.loaderUrl;
     let runtimeUrl = AWAY_EMBED_CFG.runtimeUrl;
+    const splash = AWAY_EMBED_CFG.splash;
+    const progress = AWAY_EMBED_CFG.progress;
 
-    if(!baseUrl) {
-        const source = runtimeUrl || loaderUrl;
-        baseUrl = loaderUrl.substring(0, source.lastIndexOf('/'));
-    }
-
-    if (!runtimeUrl) {
-        console.warn('AwayFL Runtime Url not defined, will be used url of base or loader!');
-
-        runtimeUrl = baseUrl  + '/runtime.js';
+    if (!baseUrl) {
+        baseUrl = new URL('.', scriptSrc).href;
     }
 
     const player = new AwayPlayerComponent();
@@ -63,6 +56,16 @@ function createRuntimeFrame(
     player.setAttribute('width', width);
     player.setAttribute('height', height);
     player.setAttribute('runtimebaseurl', baseUrl);
+
+    if (loaderUrl) {
+        player.setAttribute('loaderurl', loaderUrl);
+    }
+    if (runtimeUrl) {
+        player.setAttribute('runtimeurl', runtimeUrl);
+    }
+    if (splash === false) {
+        player.setAttribute('hidebeforeload', 'true');
+    }
 
     const id = target.id;
     const name = target.className;
